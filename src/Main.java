7c4550c9afb6cf254dd3ff3a88b2af8854d8a7d1
38180001 <jackm61@educbe.ca>
2024-09-13 10:27:07 -0600

    Finished

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,31 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner myScan = new Scanner(System.in);
-        System.out.println("Emter a 4 digit year")
+        System.out.println("Enter a 4 digit year");
         int year = myScan.nextInt();
+        int digitOne=year%10;
+        // Gets the ones digit by using the % operator, which finds the remainder of a division statement, uses int to ensure that there's no decimals
+        year=year/10;
+        //Updates the year variable to exclude the ones digit, so the four-digit number becomes a three-digit number
+        int digitTwo=year%10;
+        //variable to get the tens value through the % operator, uses int to ensure that there's no decimals
+        year=year/10;
+        // updates year to exclude the tens value, so the three-digit number becomes a two-digit number
+        int digitThree=year%10;
+        //Variable to get the hundreds digit using the % operator, uses int to ensure that there's no decimals
+        year=year/10;
+        //Updates year to exclude hundreds digit, so the two-digit number is now a one-digit number
+        int digitFour=year%10;
+        //Final variable to find thousands digit through % operator, uses int to ensure that there's no decimals
+        System.out.println(digitFour);
+        //Prints thousands place first
+        System.out.println(digitThree);
+        //Prints hundreds place second
+        System.out.println(digitTwo);
+        //Prints tens place third
+        System.out.println(digitOne);
+        //Prints ones place last
+        //Print statements from digit four to digit one, as the digit four variable has the thousands digit, while digit one has the ones place. the "println" statement is used in order to print the value, and move the cursor to the next line for the next value
         /* using MATHEMATICAL operations (no Strings), split a year
         Example: if the user enters 1989, the output would be:
         1
@@ -17,3 +40,4 @@ public class Main {
 }
 //Comment on how you tested the code to make sure it works in all situations
 //include tests that you ran
+//I tested the code by giving it different four digit values, such as 1989, 1984, 0001, 1001, 4509, 3047, 1207, and 0210, all of which worked how the program is intended to.
